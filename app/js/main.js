df7c55b17f5f39b2b35e093f5a20c1413ee09450
tramvay5401 <tramvay5401@gmail.com>
2019-12-18 11:23:10 +0200

    added page index ana drop-down menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,27 @@
-$(function(){
+$(function() {
+  $(".featured__inner").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><span class="icon-angle-right"></span></button>',
+    nextArrow:
+      '<button class="slick-arrow slick-next"><span class="icon-angle-left"></span></button>',
+  });
+  $(".slider-followers__inner").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><span class="icon-angle-right"></span></button>',
+    nextArrow:
+      '<button class="slick-arrow slick-next"><span class="icon-angle-left"></span></button>',
+  });
 
-    
 
-
+  var mixer = mixitup('.products-two__inner-box');
 });
