aebcc0ec787477619e00d533723bfcb478580c8f
tramvay5401 <tramvay5401@gmail.com>
2019-12-20 10:48:55 +0200

    added 404 contact

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,11 @@ gulp.task("style", function() {
     .src([
       "node_modules/normalize.css/normalize.css",
       "node_modules/slick-carousel/slick/slick.css",
-      "node_modules/magnific-popup/dist/magnific-popup.css"
+      "node_modules/magnific-popup/dist/magnific-popup.css",
+      "node_modules/rateyo/src/jquery.rateyo.css",
+      "node_modules/ion-rangeslider/css/ion.rangeSlider.css",
+      "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css",
+      "node_modules/jquery-form-styler/dist/jquery.formstyler.css"
     ])
     .pipe(concat("libs.min.css"))
     .pipe(cssmin())
@@ -37,7 +41,11 @@ gulp.task("script", function() {
     .src([
       "node_modules/slick-carousel/slick/slick.js",
       "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
-      "node_modules/mixitup/dist/mixitup.js"
+      "node_modules/mixitup/dist/mixitup.js",
+      "node_modules/rateyo/src/jquery.rateyo.js",
+      "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
+      "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
+      "node_modules/jquery-form-styler/dist/jquery.formstyler.js"
     ])
     .pipe(concat("libs.min.js"))
     .pipe(uglify())
